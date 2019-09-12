04a8d312e7aead943d75200bb7f557064867d571
MihaiRaducan <mihai00@yahoo.com>
2019-09-12 15:45:19 +0300

    graphics for the dice test

--- src/AppBundle/Controller/PlayerController.php
+++ src/AppBundle/Controller/PlayerController.php
@@ -46,45 +46,61 @@ class PlayerController extends Controller
     }
 
     /**
+     * checks the normallized distribution of values resulting from the mt_rand(1,6) function
      * @param $n
      * @return Response
      * @Route("/testDice/{n}", name="test_dice", methods={"GET"})
      */
     public function testDiceAction($n) {
-        $result = [];
-        for ($dice = 1; $dice <=2; $dice++) {
-            for ($face = 1; $face <=6; $face++) {
-                $result['d'.$dice][$face] = 0;
-            }
+        $dice1 = [];
+        $dice2 = [];
+        for ($face = 1; $face <=6; $face++) {
+            $dice1[$face] = 0;
+            $dice2[$face] = 0;
         }
 
-        $resultSum = [];
+        $diceSum = [];
         for ($sum = 2; $sum <= 12; $sum++) {
-            $resultSum[$sum] = 0;
+            $diceSum[$sum] = 0;
         }
 
         for ($i = 1; $i <= $n; $i++) {
             $doubleDice = $this->generateDoubleDice();
             for ($case = 1; $case <= 6; $case++) {
                 if ($doubleDice['d1'] === $case) {
-                    $result['d1'][$case]++;
+                    $dice1[$case]++;
                 }
                 if ($doubleDice['d2']=== $case) {
-                    $result['d2'][$case]++;
+                    $dice2[$case]++;
                 }
             }
             for ($ddSum = 2; $ddSum <= 12; $ddSum++) {
                 if (array_sum($doubleDice) === $ddSum) {
-                    $resultSum[$ddSum]++;
+                    $diceSum[$ddSum]++;
                 }
             }
         }
 
-        dump ($result); dump($resultSum);
+        $sumDice1 = array_sum($dice1);
+        foreach ($dice1 as $key => $value) {
+            $dice1['prob' . $key] = $value/$sumDice1*100;
+        }
+        $sumDice2 = array_sum($dice2);
+        foreach ($dice2 as $key => $value) {
+            $dice2['prob' . $key] = $value/$sumDice2*100;
+        }
+        $sumDiceSum = array_sum($diceSum);
+        foreach ($diceSum as $key => $value) {
+            $diceSum['prob' . $key] = $value/$sumDiceSum*100;
+        }
+
+        dump ($dice1); dump ($dice2); dump($diceSum);
 
         return $this->render('gametable/test.html.twig', array(
-            'result' => $result,
-            'resultSum' => $resultSum,
+            'n' => $n,
+            'dice1' => $dice1,
+            'dice2' => $dice2,
+            'diceSum' => $diceSum,
         ));
     }//TODO: some simple graphics for test.html.twig
 
